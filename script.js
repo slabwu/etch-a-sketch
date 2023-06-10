659c3f25630246d127ff8c509294bd90f97352b7
slabwu <134193743+slabwu@users.noreply.github.com>
2023-06-10 22:56:57 +0700

    Create button to change size

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const container = document.querySelector('.container');
 const containerHeight = 480;
 const containerWidth = 480;
+const sizeButton = document.querySelector('.size');
+
 let pixelHeight;
 let pixelWidth;
+let originalSize = 16;
 
-let size = prompt('Select a dimension of pixels.');
-while (size > 100) {
-    prompt('Invalid pixel dimension. Select another value.');
-}
-pixelHeight = containerHeight/size;
-pixelWidth = containerWidth/size;
+createCanvas(originalSize);
 
+function createCanvas(dimension) {
+pixelHeight = containerHeight/dimension;
+pixelWidth = containerWidth/dimension;
 
-for (i=0; i<(size**2); i++) {
+for (i=0; i<(dimension**2); i++) {
     let div = document.createElement('div')
     div.classList.add(`${i}`);
-    div.style.cssText = `height: ${pixelHeight}px; width: ${pixelWidth}px;`; 
+    div.style.cssText = `height: ${pixelHeight}px; width: ${pixelWidth}px;`;
+    div.addEventListener('mouseover', color);
     container.appendChild(div);
 }
+}
 
-const pixels = document.querySelectorAll('div');
-
-pixels.forEach(pixel => pixel.addEventListener('mouseover', color));
+let pixels = document.querySelectorAll('div');
 
 function color () {
     this.classList.add('hovered');
 }
-//container.style.backgroundColor="pink";
+
+
+
 container.style.cssText = `height: ${containerHeight}px; width: ${containerWidth}px;`; 
+
+sizeButton.addEventListener('click', changeSize);
+
+function changeSize() {
+    let size = prompt('Select a dimension of pixels.');
+    while (size > 100 || size < 1) {
+        size = prompt('Invalid pixel dimension. Select another value.');
+    }
+    pixels.forEach(pixel => pixel.remove());
+    createCanvas(size);
+    pixels = document.querySelectorAll('div');
+}
