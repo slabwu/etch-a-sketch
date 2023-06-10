bd36316713fcff43f9c6b5fa45ae306cdab773d3
slabwu <134193743+slabwu@users.noreply.github.com>
2023-06-10 21:53:02 +0700

    Add hover colour effect

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
+const maxWidth = 1000
+const maxHeight = 1000
 
 for (i=0; i<(16*16); i++) {
-    let div = document.createElement('div');
+    let div = document.createElement('div')
+    div.classList.add(`${i}`);
     container.appendChild(div);
+}
+
+const pixels = document.querySelectorAll('div');
+
+pixels.forEach(pixel => pixel.addEventListener('mouseover', color));
+
+function color () {
+    this.classList.add('hovered');
 }
 
