e33992d3d440462458e3cc913f9e6b09873da28f
slabwu <134193743+slabwu@users.noreply.github.com>
2023-06-10 22:13:18 +0700

    Allow for selection of canvas dimensions

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const container = document.querySelector('.container');
-const maxWidth = 1000
-const maxHeight = 1000
+const containerHeight = 480;
+const containerWidth = 480;
+let pixelHeight;
+let pixelWidth;
 
-for (i=0; i<(16*16); i++) {
+let size = prompt('Select a dimension of pixels.');
+while (size > 100) {
+    prompt('Invalid pixel dimension. Select another value.');
+}
+pixelHeight = containerHeight/size;
+pixelWidth = containerWidth/size;
+
+
+for (i=0; i<(size**2); i++) {
     let div = document.createElement('div')
     div.classList.add(`${i}`);
+    div.style.cssText = `height: ${pixelHeight}px; width: ${pixelWidth}px;`; 
     container.appendChild(div);
 }
 
@@ -15,4 +26,5 @@ pixels.forEach(pixel => pixel.addEventListener('mouseover', color));
 function color () {
     this.classList.add('hovered');
 }
-
+//container.style.backgroundColor="pink";
+container.style.cssText = `height: ${containerHeight}px; width: ${containerWidth}px;`; 
